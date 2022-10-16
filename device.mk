e9e9021cc6289fad762fc2073e5b5fa071b70848
Jabiyeff <cebiyevanar@gmail.com>
2022-10-16 18:34:14 +0400

    camellia: parts: Import XiaomiParts for refresh rate control
    Signed-off-by: Jabiyeff <cebiyevanar@gmail.com>

--- device.mk
+++ device.mk
@@ -384,5 +384,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,*,$(LOCAL_PATH)/configs/wifi/,$(TARGET_COPY_OUT_VENDOR)/etc/wifi)
 
+# XiaomiParts
+PRODUCT_PACKAGES += \
+    XiaomiParts
+
 # Inherit the proprietary files
 $(call inherit-product, vendor/xiaomi/camellia/camellia-vendor.mk)
