471fe88b7e15f248aafa3c4ea98166321fbf002f
Mathieu Chartier <mathieuc@google.com>
2023-02-11 10:17:52 +0000

    camellia: Speed profile services and wifi-service to reduce RAM and storage
    Change-Id: I358bb89c25dbab21725671d1e4c63e5bf117e48a
    Signed-off-by: Noah113-Allen <kingfighter2958@gmail.com>

--- device.mk
+++ device.mk
@@ -356,6 +356,9 @@ PRODUCT_SOONG_NAMESPACES += \
     hardware/mediatek \
     hardware/xiaomi
 
+# Speed profile services and wifi-service to reduce RAM and storage
+PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
+
 # Thermal
 PRODUCT_PACKAGES += \
     android.hardware.thermal-service.mediatek
