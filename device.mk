ae29296edc1f92caef3e6658a67a040850b7f9ec
bengris32 <bengris32@protonmail.ch>
2023-12-26 21:05:38 +0000

    camellia: Switch to common QTI vibrator service
    Change-Id: Ib58c8f5cd33ffbf0cf8c18ceb14c81cb21b1b2f9
    Signed-off-by: bengris32 <bengris32@protonmail.ch>
    Signed-off-by: ramshell688 <ramonzost68@gmail.com>

--- device.mk
+++ device.mk
@@ -380,8 +380,13 @@ PRODUCT_PACKAGES += \
     android.hardware.usb@1.3-service.mt6833
 
 # Vibrator
+$(call soong_config_set, vibrator, vibratortargets, vibratoraidlV2target)
+
 PRODUCT_PACKAGES += \
-    android.hardware.vibrator-service.mediatek
+    vendor.qti.hardware.vibrator.service
+
+PRODUCT_COPY_FILES += \
+    vendor/qcom/opensource/vibrator/excluded-input-devices.xml:$(TARGET_COPY_OUT_VENDOR)/etc/excluded-input-devices.xml
 
 # Wi-Fi
 PRODUCT_PACKAGES += \
