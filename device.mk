c019777e5b2723b312ad9a6574a19bc5c83fd28f
sabrina010209 <mcpe11640@gmail.com>
2024-01-10 12:42:51 -0500

    camellia: revert: Label and compile health AIDL
    Signed-off-by: sabrina010209 <mcpe11640@gmail.com>

--- device.mk
+++ device.mk
@@ -144,8 +144,9 @@ PRODUCT_PACKAGES += \
 
 # Health
 PRODUCT_PACKAGES += \
-    android.hardware.health-service.mt6833 \
-    android.hardware.health-service.mt6833-recovery
+    android.hardware.health@2.1-impl \
+    android.hardware.health@2.1-impl.recovery \
+    android.hardware.health@2.1-service
 
 # HIDL
 PRODUCT_PACKAGES += \
